6d921546226b5571ac2a5b582b75fa5e0826b5a6
suntansoker123 <sayantanthepointer@gmail.com>
2019-12-20 21:24:10 +0530

    Version 2.0 updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LessonOneComponent } from './lessons/lesson1/lesson1.component';
+import { LessonTwoComponent } from './lessons/lesson2/lesson2.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'lesson1', component: LessonOneComponent }
+  { path: 'lesson1', component: LessonOneComponent },
+  { path: 'lesson2', component: LessonTwoComponent }
   // { path: 'customers', 
   //   loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule),
   //   canLoad: [ CustomersLoadGuardService ]
